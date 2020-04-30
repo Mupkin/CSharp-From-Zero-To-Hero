d3ba974d41acefec31ff8e07ea07932653a1029e
Mupkin <jean-philippe.larochelle.1@ens.etsmtl.ca>
2020-04-29 20:21:41 -0400

    added variable

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,9 @@ namespace BootCamp.Chapter
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Hello World! 2");
+            Console.WriteLine("Hello World! 3");
+
+            var myName = Console.ReadLine();
         }
     }
 }
